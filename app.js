25ac52b6dc11c5516979a266c4e910199b661cd7
zameerSheikh <zameer@gmail.com>
2019-11-27 17:42:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+const mong = require('./mongo');
+
+var app = express();
+var bodyparser = require('body-parser');
+app.use(express.static("public"))
+
+const friends = ['Wes', 'Tony', 'Addy', 'Waseem'];
+
+app.set('view engine', 'ejs')
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.send('You got to the root url')
+})
+
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
+app.get('/friends', mong.getFriends);
+
+app.post('/addFriend', mong.addFriend)
+
+// app.get('/friends', (req, res) => {
+//     res.render('friends', {friends})
+// });
+
+// app.post('/addFriend', (req, res) => {
+//     friends.push(req.body.name);
+//     res.redirect('/friends');
+// })
+
+app.get('*', (req, res) => {
+    res.send('un oh, requested url not found.');
+})
+
+const port = process.env.port || 4000
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
---
+++ mongo.js
@@ -0,0 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://zameer:3kc05ec045@cluster0-zpyvn.mongodb.net/Friends?retryWrites=true&w=majority'
+
+const addFriend = async (req, res, next) => {
+    let {name} = req.body;
+    const client = new MongoClient(url);
+
+    try {
+       await client.connect();
+       const db = client.db();
+       const result = db.collection('friends_list').insertOne(name);
+    } catch (error) {
+        return res.json({message: 'Could not add your friend 😟'})
+    }
+    client.close();
+
+    res.json(name)
+};
+
+const getFriends = async (req, res, next) => {
+    const client = new MongoClient(url);
+    let friendsList;
+    try {
+        await client.connect();
+        const db = client.db();
+        friendsList = await db.collection('friends_list').find().toArray();
+        
+    } catch (error) {
+        console.log('Could not fetch your friends list. 😔');
+        // res.render('friends', {friends: friendsList || []})
+    }
+    client.close();
+
+    res.json(friendsList || [])
+    // res.render('friends', {friends: friendsList})
+}
+
+module.exports = {
+    addFriend,
+    getFriends
+}
